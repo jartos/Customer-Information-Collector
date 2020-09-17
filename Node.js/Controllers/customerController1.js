3ea69a5629795754b62a8e32136d0846811facf8
Jarno Tossavainen <jarnotossavainen83@gmail.com>
2020-09-17 17:07:58 +0300

    CustomerController line 158 changed status 200 to 205

--- Node.js/Controllers/customerController1.js
+++ Node.js/Controllers/customerController1.js
@@ -2,31 +2,31 @@
 module.exports = {
 
     fetch: (req, res) => {         //
-       
-    
+
+
         let params = [];
         console.log("/asiakas. REQ:", req.query);
 
         let query = "SELECT a.AVAIN, date_format(MUUTOSPVM, '%T %d.%m.%Y') as MUUTETTUMUUTOSPVM, MUUTOSPVM, a.NIMI, a.OSOITE, a.POSTINRO, a.POSTITMP, DATE_FORMAT(DATE(LUONTIPVM), ' %d.%m.%Y') as LUONTIPVM, ASTY_AVAIN, asty.SELITE FROM Customer.asiakas a JOIN Customer.asiakastyyppi asty ON a.ASTY_AVAIN = asty.AVAIN WHERE 1=1";
-        
+
         if (req.query.avain != null) {
-            
+
             query = query + " AND a.AVAIN like ?" ;
             params.push(req.query.avain);
-        
+
         }
         if (req.query.nimi != null) {
-            
+
             query = query + " AND a.NIMI like ? '%'" ;
             params.push(req.query.nimi);
         }
         if (req.query.osoite != null) {
-            
+
             query = query + " AND a.OSOITE like ? '%'";
             params.push(req.query.osoite);
         }
         if (req.query.asiakastyyppi != null) {
-            
+
             query = query + " AND asty.SELITE like ? ";
             params.push(req.query.asiakastyyppi);
         }
@@ -35,10 +35,10 @@ module.exports = {
             res.statusCode = 202;
             res.json({ status: "Virhe", msg:"Checkbox ruksattu"});
         }
- 
+
         console.log("query:" + query);
         connection.query(query, params, function (error, result, fields) {
-    
+
             if (error) {
                 console.log("Virhe", error);
                 res.statusCode = 400;
@@ -55,33 +55,33 @@ module.exports = {
     create: (req, res) => {                               // TEHT 6
 
         console.log("CREATE: ", req.body);
-        
+
 
         let q = "INSERT INTO asiakas (NIMI, OSOITE, POSTINRO, POSTITMP, LUONTIPVM, ASTY_AVAIN) VALUES(?, ?, ?, ?, ?, ?)";
-    
-      
+
+
         if (req.body.NIMI == "") {                                       // TEHT 7
-            
+
             res.json({ status: "NOT OK", msg: "Nimi puuttuu" });
         }
         if (req.body.OSOITE == "") {
-            
+
             res.json({ status: "NOT OK", msg: "Osoite puuttuu" });
         }
         if (req.body.POSTINRO == "") {
-            
+
             res.json({ status: "NOT OK", msg: "Postinumero puuttuu" });
         }
         if (req.body.POSTITMP == "") {
-            
+
             res.json({ status: "NOT OK", msg: "Postitoimipaikka puuttuu" });
         }
         if (req.body.LUONTIPVM == "") {
-            
+
             res.json({ status: "NOT OK", msg: "Luontipvm puuttuu" });
         }
         if (req.body.ASTY_AVAIN == "") {
-            
+
             res.json({ status: "NOT OK", msg: "Asty_avain puuttuu" });
         }
 
@@ -90,7 +90,7 @@ module.exports = {
 
         console.log("query:" + q);
         connection.query(q, [req.body.NIMI, req.body.OSOITE, req.body.POSTINRO, req.body.POSTITMP, req.body.LUONTIPVM, req.body.ASTY_AVAIN], function (error, result, fields) {
-    
+
             if (error) {
                 console.log("Virhe", error);
                 res.statusCode = 200;   // 400, 500
@@ -101,25 +101,25 @@ module.exports = {
                 console.log("RESULT:", result);
                 res.json({ avain: result.insertId, status: "OK", msg: "Lisätty" })
             }
-        
+
         });
     }
-    },    
+    },
 
     update : (req, res) => {
 
-        console.log("UPDATE params: ", req.body.avain);     
+        console.log("UPDATE params: ", req.body.avain);
         console.log("params: ", req.params);
-    
+
         let q = "UPDATE Customer.asiakas SET NIMI = ?, OSOITE = ?, POSTINRO = ?, POSTITMP = ?, LUONTIPVM = ?, ASTY_AVAIN = ?, MUUTOSPVM = ? WHERE AVAIN = ?;"
-    
+
         console.log("query:" + q);
         if (req.body.avain == null) {
             console.log("Ei avainta");
         }
-    
+
         connection.query(q, [req.body.NIMI, req.body.OSOITE, req.body.POSTINRO, req.body.POSTITMP, req.body.LUONTIPVM, req.body.ASTY_AVAIN, req.body.MUUTOSPVM, req.body.avain], function (error, result, fields) {
-    
+
             if (error) {
                 console.log("Virhe", error);
                 res.statusCode = 400;   // 400, 500
@@ -144,7 +144,7 @@ module.exports = {
         connection.query(q, [req.body.avain], function (error, result, fields) {
 
             if (error) {
-                console.log("Virhe", error);        
+                console.log("Virhe", error);
                 res.statusCode = 400;   // 400, 500
                 res.json({ status: "DELETE FAIL", msg: "ERROR" });
             }
@@ -155,7 +155,7 @@ module.exports = {
             }
 
             else {
-                res.statusCode = 200;
+                res.statusCode = 205;
                 console.log("RESULT:", result);
                 res.json({ status: "OK", msg: "Poistettu"})
             }
